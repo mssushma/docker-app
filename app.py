2273446f7b3589503b4bf7c3aae15e63490a3a2b
mssushma <87899596+mssushma@users.noreply.github.com>
2026-07-20 17:20:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,5 +10,12 @@ def home():
 def health():
     return "healthy"
 
+@app.route("/version")
+def version():
+    return "1.0"
+ 
+def calculate():
+    return 10 + 20
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

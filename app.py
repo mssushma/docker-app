f81f9532d52f0ce76b70ba381a1a7d8b2c82bf03
mssushma <87899596+mssushma@users.noreply.github.com>
2026-07-20 17:35:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,5 @@ app = Flask(__name__)
 def home():
     return "Containerized Application Running Successfully"
 
-@app.route("/health")
-def health():
-    return "healthy"
-
-@app.route("/version")
-def version():
-    return "1.0"
-
-def calculate():
-    return 10 + 20
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
